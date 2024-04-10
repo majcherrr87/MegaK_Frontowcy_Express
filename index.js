236e50f8dfd4f4bdf37b91a82154e123a9054e79
Adrian Majcher <majcherrr87@gmail.com>
2024-04-10 12:54:01 +0200

    learn cookie and path and params

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
-const { join } = require("path");
+const { readFile, writeFile } = require("fs").promises;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.cookie("ciastko", "czekoladowe bez cukru").send("hello world!");
+app.get("/name/set/:name", async (req, res) => {
+  const name = req.params.name;
+  await writeFile("name.txt", name, "utf-8");
+  res.send(`Imie zostało zapisane ${name}`);
+});
+
+app.get("/name/show", async (req, res) => {
+  showName = await readFile("name.txt", "utf-8");
+  res.send(`Zapisane Imię to ${showName}`);
+});
+
+app.get("/name/check", async (req, res) => {
+  try {
+    await readFile("name.txt", "utf-8");
+    res.send("There is a name saved");
+  } catch (e) {
+    res.send("There is no name");
+  }
+});
+
+app.get("/:a/:b", (req, res) => {
+  const { a, b } = req.params;
+  const sum = Number(a) + Number(b);
+  res.send(`${a} + ${b} = ${sum}`);
 });
 
 app.listen(3000);
