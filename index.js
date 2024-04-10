368520860a90740fdae7a654f1ce651c0d0b4c68
Adrian Majcher <majcherrr87@gmail.com>
2024-04-10 17:48:22 +0200

    cookieRouter

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const { nameRouter } = require("./routes/name");
 const { voteRouter } = require("./routes/voteRouter");
 const { calcRouter } = require("./routes/calc");
+const { cookieRouter } = require("./routes/cookie");
 
 const app = express();
-app.use(express.static("public"));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.use("/name", nameRouter);
 app.use("/vote", voteRouter);
 app.use("/calc", calcRouter);
+app.use("/cookie", cookieRouter);
 
 app.listen(3000, "localhost");
---
+++ routes/cookie.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const cookieRouter = express.Router();
+
+cookieRouter;
+
+module.exports = {
+  cookieRouter,
+};
