01574b7a895d95b8dbd41134ffb5a1b7b22fbe61
Adrian Majcher <majcherrr87@gmail.com>
2024-04-11 15:07:43 +0200

    learn HBS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const hbs = require("express-handlebars");
 
 const { nameRouter } = require("./routes/name");
 const { voteRouter } = require("./routes/voteRouter");
@@ -10,10 +11,21 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.engine(".hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 app.use("/name", nameRouter);
 app.use("/vote", voteRouter);
 app.use("/calc", calcRouter);
 app.use("/cookie", cookieRouter);
+
+app.get("/hi", (req, res) => {
+  res.render("home");
+});
 
 app.listen(3000, "localhost");
--- routes/cookie.js
+++ routes/cookie.js
@@ -2,7 +2,21 @@ const express = require("express");
 
 const cookieRouter = express.Router();
 
-cookieRouter;
+cookieRouter
+  .post("/set", (req, res) => {
+    const { name } = req.body;
+    res
+      .cookie("name", name, { maxAge: 30 * 24 * 60 * 60 * 1000 })
+      .send(`Imię ${name} zostało zapisane`);
+  })
+  .get("/show", (req, res) => {
+    const { name } = req.cookies;
+    res.send(name ? `zapisane imię to ${name}` : "Brak imienia");
+  })
+  .get("/check", (req, res) => {
+    const { name } = req.cookies;
+    res.send(name ? "Imię jest zapamiętane" : "Nie ma takirgo imienia");
+  });
 
 module.exports = {
   cookieRouter,
